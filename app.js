a7802fac4ce749996b073fd72111f50ad770484a
David Moya <davidmoya440@gmail.com>
2021-07-15 07:21:31 -0500

    Rama dev

--- app.js
+++ app.js
@@ -21,3 +21,6 @@ app.get('/register',(req,res) =>{
 app.get('/login',(req,res) =>{
     res.sendFile(__dirname + '/views/login.html');
 });
+app.get('/prueba',(req,res) =>{
+    res.sendFile(__dirname + '/views/prueba.html');
+});
