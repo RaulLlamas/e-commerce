2133fb1b305301ff5886c2f27d85533525226370
rebecajs <log.juarez.sanchez.rebeca@gmail.com>
2021-09-16 02:34:57 -0500

    structure sequelize

---
+++ structure.sql
@@ -0,0 +1,178 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: structure
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `addresses`
+--
+
+DROP DATABASE IF EXISTS 'structure';
+CREATE DATABASE 'structure';
+USE 'structure';
+
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `addresses` (
+  `Id_addresses` int(11) NOT NULL AUTO_INCREMENT,
+  `Street` varchar(100) NOT NULL,
+  `Number_ext` tinyint(4) NOT NULL,
+  `Colony` varchar(100) NOT NULL,
+  `Number_int` tinyint(4) DEFAULT NULL,
+  `Reference` text DEFAULT NULL,
+  PRIMARY KEY (`Id_addresses`),
+  UNIQUE KEY `Addresses_un` (`Street`,`Number_ext`,`Colony`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `addresses`
+--
+
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `Id_categories` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  PRIMARY KEY (`Id_categories`),
+  UNIQUE KEY `Categories_un` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `Id_products` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Price` tinytext NOT NULL,
+  `Id_Categories` varchar(10) NOT NULL,
+  `Description` text DEFAULT NULL,
+  `Image` mediumblob DEFAULT NULL,
+  PRIMARY KEY (`Id_products`),
+  UNIQUE KEY `Products_un` (`Name`,`Price`) USING HASH,
+  KEY `products_FK` (`Id_Categories`),
+  CONSTRAINT `products_FK` FOREIGN KEY (`Id_Categories`) REFERENCES `categories` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shopping cart`
+--
+
+DROP TABLE IF EXISTS `shopping cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shopping cart` (
+  `Id_categories` int(11) NOT NULL AUTO_INCREMENT,
+  `Id_users` int(11) NOT NULL,
+  `Number_of_items` int(11) NOT NULL,
+  `Id_products` int(11) NOT NULL,
+  PRIMARY KEY (`Id_categories`),
+  KEY `shopping_cart_FK` (`Id_users`),
+  KEY `shopping_cart_FK_1` (`Id_products`),
+  CONSTRAINT `shopping_cart_FK` FOREIGN KEY (`Id_users`) REFERENCES `users` (`Id_users`),
+  CONSTRAINT `shopping_cart_FK_1` FOREIGN KEY (`Id_products`) REFERENCES `products` (`Id_products`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shopping cart`
+--
+
+LOCK TABLES `shopping cart` WRITE;
+/*!40000 ALTER TABLE `shopping cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shopping cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `Id_users` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Telephone` int(11) DEFAULT NULL,
+  `Password` varchar(100) NOT NULL,
+  `Birthday_date` date DEFAULT NULL,
+  `Id_Addresses` int(11) NOT NULL,
+  `Image` mediumblob DEFAULT NULL,
+  PRIMARY KEY (`Id_users`),
+  UNIQUE KEY `Users_un` (`Email`),
+  KEY `users_FK` (`Id_Addresses`),
+  CONSTRAINT `users_FK` FOREIGN KEY (`Id_Addresses`) REFERENCES `addresses` (`Id_addresses`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'structure'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-15 18:51:16
