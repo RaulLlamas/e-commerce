15f66c59a505cfa4a3d9cda8811bc68096b7ca08
RaulLlamas <rauldllamas@gmail.com>
2021-10-22 19:39:02 -0500

    Update structure db 2

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "llamas2456",
+    "password": "-",
     "database": "rogers",
     "host": "127.0.0.1",
     "dialect": "mysql"
