02bb694f48c91dad671486334e952253c422222e
RaulLlamas <rauldllamas@gmail.com>
2021-10-26 21:08:53 -0500

    fech totales

--- react/dashboard/src/components/LastProduct/index.js
+++ react/dashboard/src/components/LastProduct/index.js
@@ -1,23 +1,58 @@
 import React from "react";
+import {Component} from 'react';
 
 import Img from '../../assets/images/img-hamburguesa.png';
 
 import "./LastProduct.css";
 
-const LastProduct = () => (
-    <div className='LastProduct'>
-            <div class="ImageDetail">
-                <img src={Img} alt="image-product"/>
-            </div>
-            <div class="ProductInfo">
-                <p class="ProductName">Hamburguesa</p>
-                <p class="ProductPrice" >$150</p>
-                <p class="ProductCate">Por lo que vienes</p>
-                <p class="ProductDescTitle">Descripcion:</p>
-                <p class="ProductDescription">Carne de puerco deshebrada mezclada con salsa BBQ, con una mezcla de quesos cheddar y mozzarella gratinados.</p>
+class LastProduct extends Component{
     
+    constructor(){
+        super()
+        this.state={
+            total: 0,
+            producto : {}
+        }
+    }
+
+    componentDidMount(){
+        fetch('/api/products')
+        .then(respuesta => {
+            return respuesta.json()
+        })
+        .then(products =>{
+            this.setState({total: products.total})
+        })
+        .catch(error => console.log(error))
+
+        fetch('/api/products/'+(this.state.total-1))
+        .then(respuesta => {
+            return respuesta.json()
+        })
+        .then(product =>{
+            console.log(product);
+            this.setState({producto: product})
+        })
+        .catch(error => console.log(error))
+    }
+
+    render(){
+        return (
+            <div className='LastProduct'>
+                <div class="ImageDetail">
+                    <img src={Img} alt="image-product"/>
+                </div>
+                <div class="ProductInfo">
+                    {/* <p class="ProductName">{this.state.producto.name}</p>
+                    <p class="ProductPrice" >{this.state.producto.price}+</p>
+                    <p class="ProductCate">{this.state.producto.categoria.name}</p>
+                    <p class="ProductDescTitle">Descripcion:</p>
+                    <p class="ProductDescription">{this.state.producto.description}</p>
+         */}
+                </div>
             </div>
-    </div>
-);
+        )
+    }
+}
 
 export default LastProduct;
--- react/dashboard/src/components/Totales/index.js
+++ react/dashboard/src/components/Totales/index.js
@@ -1,15 +1,52 @@
 import React from "react";
+import {Component} from 'react';
 
 import Total from '../Total';
 
 import "./Totales.css";
 
-const Totales = () => (
-    <div className='WrapperTotales'>
-        <Total title='Products' number='15'/>
-        <Total title='Categorias' number='3'/>
-        <Total title='Products' number='15'/>
-    </div>
-);
+class Totales extends Component{
+    
+    constructor(){
+        super()
+        this.state={
+            totalProductos : 0,
+            totalCate : 0,
+            totalUser: 0
+        }
+
+    }
+
+    componentDidMount(){
+        fetch('/api/products')
+        .then(respuesta => {
+            return respuesta.json()
+        })
+        .then(products =>{
+            this.setState({totalProductos: products.total})
+            this.setState({totalCate: Object.keys(products.totalbyCategory).length})
+        })
+        .catch(error => console.log(error))
+
+        fetch('/api/users')
+        .then(respuesta => {
+            return respuesta.json()
+        })
+        .then(users =>{
+            this.setState({totalUser: users.total})
+        })
+        .catch(error => console.log(error))
+    }
+
+    render(){
+        return (
+        <div className='WrapperTotales'>
+            <Total title='Products' number={this.state.totalProductos}/>
+            <Total title='Categorias' number={this.state.totalCate}/>
+            <Total title='Products' number={this.state.totalUser}/>
+        </div>
+        )
+    }
+}
 
 export default Totales;
